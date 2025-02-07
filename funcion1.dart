74c4d238729674f64d213c4db2681f5b5821ca1c
Ernesto Alexander <164534106+Ernesto-Larrondo-1070@users.noreply.github.com>
2025-02-07 16:47:44 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+// Funcion que no develve valor
+// Funcion saludar
+void saludar(){
+  print("hola mundo");
+}
+
+void EscribirNombre(){
+  print("Ernesto Larrondo");
+} 
+
+void main(){
+  // Ernesto Larrondo mat: 1070
+  // llamar a la funcion saludar
+  print ("Ernesto Larrondo");
+  print ("Llamado a la funcion saludar");
+  saludar();
+  print ("Llamado a la funcion EscribirNombre");
+    EscribirNombre();  
+    
+}
