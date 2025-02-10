8bbd077ccb5e7bb53d565467436216246b7a4b05
Ernesto Alexander <164534106+Ernesto-Larrondo-1070@users.noreply.github.com>
2025-02-10 18:04:25 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,23 @@
+void suma(int a, int b){
+  print("La suma de $a + $b es: ${a+b}");
+}
+
+void resta(int a, int b){
+  print("La resta de $a - $b es: ${a-b}");
+}
+
+void multiplicacion(int a, int b){
+  print("La multiplicacion de $a * $b es: ${a*b}");
+}
+
+void division(int a, int b){
+  print("La division de $a / $b es: ${a/b}");
+}
+// funcion principal
+void main(){
+  print("Ernesto Larrondo");
+  suma(10, 5);
+  resta(10, 5);
+  multiplicacion(10, 5);
+  division(10, 5);
+}
