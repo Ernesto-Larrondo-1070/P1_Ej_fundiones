77e52e8d0195319155c27c85533b712e2a2b82a2
Ernesto Alexander <164534106+Ernesto-Larrondo-1070@users.noreply.github.com>
2025-02-11 20:21:44 +0000

    Problema9

---
+++ problema9.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Lista de números enteros desordenados
+  List<int> numeros = [5, 3, 8, 1, 2, 7, 4, 10, 6, 9];
+
+  // Imprimir la lista original
+  print("Lista original: $numeros");
+
+  // Ordenar la lista de menor a mayor
+  numeros.sort();
+
+  // Imprimir la lista ordenada
+  print("Lista ordenada: $numeros");
+}
---
+++ recursos/problema9.md
@@ -0,0 +1,4 @@
+Escribe un programa que tome una lista de números enteros (int) y la ordene de menor a mayor.
+
+Resultados:
+![alt text](image-4.png)
